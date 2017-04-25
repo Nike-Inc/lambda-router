3983cf74d99c1d9e132c6bcbb0b0d9f91e5901fc
kninn1 <kevin.ninnemann@nike.com>
2017-04-25 14:51:40 -0700

    Fixing the proxy integration routing.
    Kevin Ninnemann; kninn1

--- src/index.js
+++ src/index.js
@@ -47,7 +47,8 @@ Router.prototype.log = function () {
 
 function getRoute (self, event) {
   let route = self.routes.find(route => {
-    return (event.path === route.path || event.resourcePath === route.path) && event.method === route.method
+    return (event.path === route.path || event.resourcePath === route.path) &&
+      (event.method === route.method || event.httpMethod === route.method)
   })
 
   return route || self.unknownRoute || { handler: defaultUnknownRoute }
